ecbf4a257b71a1ecb2e6a2d02d73cf067aab5dcb
DebasishMohanta <debasish2018rta@gmail.com>
2023-06-29 07:29:11 +0530

    Adding Sing up and Password Change View

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,12 @@ from . import views
 app_name = "authentication"
 
 urlpatterns = [
-    path("login/",views.login_view,name="login"),
-    path("logout/",views.logout_view,name='logout'),
+    # path("login/",views.login_view,name="login"),
+    path("login/",views.CustomLoginView.as_view(),name="login"),
+    # path("logout/",views.logout_view,name='logout'),
+    path("logout/",views.Logout.as_view(),name='logout'),
+    path("change-password/",views.ChangePassword.as_view(),name="change_password"),
+    path("change-password-done/",views.ChangePasswordDone.as_view(),name="password_change_done"),
+    path("register/",views.signup_view,name="signup"),
 ]
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,34 +1,75 @@
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm,BootstrapPasswordChangeForm,BootstrapAuthenticationForm,SignupForm
 from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponseRedirect,HttpResponse
-from django.urls import reverse
-# Create your views here.
+from django.urls import reverse,reverse_lazy
+
+from django.contrib.auth.views import LoginView,LogoutView,PasswordChangeView,PasswordChangeDoneView
+
+from django.contrib.auth.decorators import login_required
 
 
-def login_view(request):
+# Using generic view
+class CustomLoginView(LoginView):
+    template_name="authentication/login.html"
+    form_class = BootstrapAuthenticationForm
+    redirect_authenticated_user = True
+
+
+# def login_view(request):
+#     if request.user.is_authenticated:
+#         return HttpResponseRedirect(reverse("blog:index"))
+#     form = LoginForm()
+#     message = ''
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             user = authenticate(request,username=form.cleaned_data['username'],password = form.cleaned_data['password'])
+
+#             if user is not None:
+#                 login(request,user)
+#                 message = f'You are logged in as {user.username}'
+#                 return HttpResponseRedirect(reverse("blog:index"))
+#             else:
+#                 message = 'Invalid Credentials! Try Again'
+#     context = {
+#         "form": form,
+#         "message": message 
+#     }
+#     return render(request,"authentication/login.html",context)
+
+
+# def logout_view(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse('blog:index'))
+
+class Logout(LogoutView):
+    pass
+
+
+class ChangePassword(PasswordChangeView):
+    form_class = BootstrapPasswordChangeForm
+    template_name = "authentication/passwordchange.html"
+    success_url = reverse_lazy('authentication:password_change_done')
+
+
+
+class ChangePasswordDone(PasswordChangeDoneView):
+    template_name = "authentication/passwordchangedone.html"
+
+
+def signup_view(request):
     if request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("blog:index"))
-    form = LoginForm()
-    message = ''
+        return HttpResponseRedirect(reverse('blog:index'))
+    form = SignupForm()
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            user = authenticate(request,username=form.cleaned_data['username'],password = form.cleaned_data['password'])
-
-            if user is not None:
-                login(request,user)
-                message = f'You are logged in as {user.username}'
-                return HttpResponseRedirect(reverse("blog:index"))
-            else:
-                message = 'Invalid Credentials! Try Again'
+            user = form.save()
+            login(request,user)
+            return HttpResponseRedirect(reverse('blog:index'))
+        
     context = {
-        "form": form,
-        "message": message 
+        "form": form
     }
-    return render(request,"authentication/login.html",context)
-
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('blog:index'))
+    return render(request,'authentication/signup.html',context=context)
